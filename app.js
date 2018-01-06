24651853f925fdf13170366af695ae6d6622536f
zzmartinj@gmail.com <jeremymartin@MacBook-Air.local>
2018-01-06 09:31:50 -0500

    basic little "c" in crud for widgets to learn posts in express

--- app.js
+++ app.js
@@ -21,9 +21,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static('static'))
 
 app.use('/', index);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+//I would normally never do this by putting a globa in the routing. FOR DEMO purposes only
+var widgets = [{
+  id: 1,
+  name: 'test',
+  price: 100.00,
+  desc: 'Test'
+}]
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 //test router
-router.get('/test', function(req,res,next){
-  res.render('index', {title: 'Tester!'},function(err, data){
-    if (!err){ //Very basic error checking!
+router.get('/test', function (req, res, next) {
+  res.render('index', { title: 'Tester!' }, function (err, data) {
+    if (!err) { //Very basic error checking!
       console.log(err);
       res.write(data);
       res.end();
@@ -18,9 +26,22 @@ router.get('/test', function(req,res,next){
 });
 
 //regular express route
-router.get('/jeremy/:id', function(req,res,next){
+router.get('/jeremy/:id', function (req, res, next) {
   res.write('you got Jeremy with an id: ' + req.params.id);
   res.end();
 })
+
+router.post('/widgets/add', function (req, res, next) {
+  var wlength = widgets.length;
+  //Add to our in-memory array
+  widgets[wlength] = {
+    id: wlength,
+    name: req.body.widgetname,
+    price: parseFloat(req.body.widgetprice),
+    desc: req.body.widgetdesc
+  };
+  console.log(widgets[wlength]);
+  res.send('Widget created! : ' + widgets[wlength]);
+});
 
 module.exports = router;
